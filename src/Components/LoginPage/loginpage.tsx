8bc335582c6f88ffa8191150db74d45d6b7d3ee1
jacob <jacob.dekok01@gmail.com>
2023-07-12 10:00:52 -0700

    Login Page Redesign

--- src/Components/LoginPage/loginpage.tsx
+++ src/Components/LoginPage/loginpage.tsx
@@ -2,17 +2,18 @@ import { Row, Col, Form } from 'react-bootstrap';
 import './loginpage.css';
 
 export default function LoginPage() {
+    const login = () => {
+        console.log('Login');
+    }
     return (
-        <div>
+        <div className='loginSpacing'>
             <Row>
-                <Col xs={8}>
+                <Col>
                     <div className='titleDiv'>
                         <h1 className='titleTxt'>COOP</h1>
-                        <h3 className='titleSubTxt'>A Warm Welcome Here</h3>
                     </div>
-                </Col>
-                <Col xs={4} className='formCol'>
-                    <Form>
+                    <Form className='loginForm'>
+                        <h3 className='titleSubTxt'>A Warm Welcome Here</h3>
                         <Form.Group className="m-3" controlId="email">
                             <Form.Label className='formLabel'>
                                 Email Address
@@ -25,6 +26,9 @@ export default function LoginPage() {
                             </Form.Label>
                             <Form.Control className='formInput' type="password" placeholder="Password" />
                         </Form.Group>
+                        <div className='btnDiv'>
+                            <button className='loginBtn'>Login</button>
+                        </div>
                     </Form>
                 </Col>
             </Row>
